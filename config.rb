b2e271b767b129f6ef2460a856d1ecd913fb7447
Robert F. Nash <robert@robertnash.net>
2013-03-18 00:00:33 -0400

    set blog default layout to page, disabled pagenation

--- config.rb
+++ config.rb
@@ -9,22 +9,20 @@ activate :blog do |blog|
   # blog.permalink = ":year/:month/:day/:title.html"
   # blog.sources = ":year-:month-:day-:title.html"
   # blog.taglink = "tags/:tag.html"
-  # blog.layout = "layout"
+  blog.layout = "page"
   # blog.summary_separator = /(READMORE)/
   # blog.summary_length = 250
   # blog.year_link = ":year.html"
   # blog.month_link = ":year/:month.html"
   # blog.day_link = ":year/:month/:day.html"
   # blog.default_extension = ".markdown"
-
   blog.tag_template = "tag.html"
   blog.calendar_template = "calendar.html"
-
-  # blog.paginate = true
-  # blog.per_page = 10
-  # blog.page_link = "page/:num"
+  blog.paginate = false
 end
 
+activate :directory_indexes
+page "/404.html", directory_index: false
 page "/feed.xml", :layout => false
 
 set :css_dir, "css"
